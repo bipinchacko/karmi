39e4486dab56434c9512eb2abb55b4b95c0cd5af
bipinchacko <bipinchacko2014@gmail.com>
2021-04-10 17:12:55 +0530

    one_to_one_chat_text2

--- chat/views.py
+++ chat/views.py
@@ -5,53 +5,28 @@ from .models import *
 from django.db.models import Q
 import json
 from django.shortcuts import get_object_or_404
+from django.core import serializers
 
 
 # Create your views here.
 @login_required
 def chatroom(request,pk:int):
-    # othrer_user = get_object_or_404(CustomUser, pk=pk)
-    # messages = Message.objects.filter(
-    #     Q(receiver=request.user, sender=othrer_user)
-    # )
-    # messages.update(seen=True)
-    # messages = messages | Message.objects.filter(Q(receiver=othrer_user, sender=request.user))
-    # return render(request,"chat/chatroom.html",{"othrer_user":othrer_user,"messages":messages})
+    current_user = request.user.id
     other_user = get_object_or_404(CustomUser, pk=pk)
     messages = Message.objects.filter(
         Q(receiver=request.user, sender=other_user)
     )
     messages.update(seen=True)
     messages = messages | Message.objects.filter(Q(receiver=other_user, sender=request.user) )
-    return render(request, "chat/chatroom.html", {"other_user": other_user, "messages": messages})
+    return render(request, "chat/onetoonechat.html", {"other_user": other_user, "messages": messages,"current_user":current_user})
 @login_required
 def ajax_load_messages(request,pk):
-    # othrer_user = get_object_or_404(CustomUser, pk=pk)
-    # messages = Message.objects.filter(seen=False).filter(
-    #     Q(receiver=request.user, sender=othrer_user)
-    # )
-    # messages.update(seen=True)
-    # message_list = [{
-    #     "sender":message.sender.first_name,
-    #     "message":message.message,
-    #     "sent":message.sender == request.user
-    # } for message in messages]
-    # messages.update(seen=True)
-    # if request.method == "POST":
-    #     message = json.loads(request.body)
-    #     m = Message.objects.create(receiver=othrer_user, sender=request.user, message=message)
-    #     message_list.append({
-    #         "sender":request.user.first_name,
-    #         "message":m.message,
-    #         "sent":True
-    #     })
-    # return JsonResponse(message_list, safe=False)
     other_user = get_object_or_404(CustomUser, pk=pk)
     messages = Message.objects.filter(seen=False).filter(
         Q(receiver=request.user, sender=other_user)
     )
     message_list = [{
-        "sender": message.sender.first_name,
+        "sender": message.sender.id,
         "message": message.message,
         "sent": message.sender == request.user
     } for message in messages]
@@ -61,7 +36,7 @@ def ajax_load_messages(request,pk):
         message = json.loads(request.body)
         m = Message.objects.create(receiver=other_user, sender=request.user, message=message)
         message_list.append({
-            "sender": request.user.first_name,
+            "sender": request.user.id,
             "message": m.message,
             "sent": True,
         })
