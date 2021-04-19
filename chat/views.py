2b08c3c3b3ee9648be9dd21ef753f163f1d94139
bipinchacko <bipinchacko2014@gmail.com>
2021-04-19 17:22:16 +0530

    one_to_one_chat_6

--- chat/views.py
+++ chat/views.py
@@ -28,29 +28,39 @@ def chatroom(request,pk:int):
     return render(request, "chat/onetoonechat.html",context)
 @login_required
 def ajax_load_messages(request,pk):
+    message_list = []
     other_user = get_object_or_404(CustomUser, pk=pk)
-    messages = Message.objects.filter(seen=False).filter(
-        Q(receiver=request.user, sender=other_user)
-    )
-    message_list = [{
-        "sender": message.sender.id,
-        "message": message.message,
-        "sent": message.sender == request.user,
-        "file":message.files.name
-    } for message in messages]
-    messages.update(seen=True)
-    
+    messages = Message.objects.filter(seen=False).filter(Q(receiver=request.user, sender=other_user))
+    for message in messages:
+        if message.files == '':
+            message_list = [{
+                "sender": message.sender.id,
+                "message": message.message,
+                "sent": message.sender == request.user,
+                "file": ''
+                
+            } ]
+        else:
+            message_list = [{
+                "sender": message.sender.id,
+                "message": message.message,
+                "sent": message.sender == request.user,
+                "file":message.files.url
+            } ]
+        print(message_list)
+        messages.update(seen=True)
+    # print(message_list)
     if request.method == "POST":
         a = request.POST.get('message')
         b = request.FILES.get('upload')
-        if a == null and b == None:
+        if a == '' and b == None:
             print("null")
         else:
             m = Message.objects.create(receiver=other_user, sender=request.user, message=a, files=b)
-        message_list.append({
-            "sender": request.user.id,
-            "message": m.message,
-            "sent": True,
-            "file":m.files.name,
-        })
+            message_list.append({
+                "sender": request.user.id,
+                "message": m.message,
+                "sent": True,
+                "file":m.files.name,
+            })
     return JsonResponse(message_list, safe=False)
